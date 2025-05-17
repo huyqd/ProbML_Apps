2ddc1505dd92e759acc03169a557436b835a9a58
Huy Dang <h33662@eon.com>
2025-05-17 19:42:02 +0200

    Add relevant notebooks

---
+++ exponential_families.py
@@ -0,0 +1,133 @@
+import abc
+from numpy.typing import ArrayLike
+import jax.numpy as jnp
+from functools import cached_property, partial
+
+
+class ExponentialFamily(abc.ABC):
+    @abc.abstractmethod
+    def sufficient_statistics(self, x: ArrayLike | jnp.ndarray, /) -> jnp.ndarray:
+        """Signature `(D)->(P)`"""
+
+    @abc.abstractmethod
+    def log_base_measure(self, x: ArrayLike | jnp.ndarray, /) -> jnp.ndarray:
+        """Signature `(D)->()`"""
+
+    @abc.abstractmethod
+    def log_partition(self, parameters: ArrayLike | jnp.ndarray, /) -> jnp.ndarray:
+        """Signature `(P)->()`"""
+
+    def parameters_to_natural_parameters(
+        self, parameters: ArrayLike | jnp.ndarray, /
+    ) -> jnp.ndarray:
+        """Signature `(P)->(P)`
+        In some EF's, the canonical parameters are
+        actually a transformation of the natural parameters.
+        In such cases, this method should be overwritten to
+        provide the inverse transformation.
+        """
+        return jnp.asarray(parameters)
+
+    def logpdf(
+        self, x: ArrayLike | jnp.ndarray, parameters: ArrayLike | jnp.ndarray, /
+    ) -> jnp.ndarray:
+        """Signature `(D),(P)->()`
+
+        log p(x|parameters)
+            = log h(x) + sufficient_statistics(x) @ natural_parameters - log Z(natural_parameters)
+            = log base measure + linear term - log partition
+        """
+        x = jnp.asarray(x)
+        log_base_measure = self.log_base_measure(x)
+        natural_parameters = self.parameters_to_natural_parameters(parameters)
+        linear_term = (
+            self.sufficient_statistics(x)[..., None, :] @ natural_parameters[..., None]
+        )[..., 0, 0]
+        log_partition = self.log_partition(parameters)
+
+        return log_base_measure + linear_term - log_partition
+
+    def conjugate_log_partition(
+        self, alpha: ArrayLike | jnp.ndarray, nu: ArrayLike | jnp.ndarray, /
+    ) -> jnp.ndarray:
+        """The log partition function of the conjugate exponential family.
+        Signature `(P),()->()`
+
+        If (and only if) this is available, it allows analytic construction of the conjugate prior
+        (and thus analytic posterior inference).
+        """
+        raise NotImplementedError()
+
+    def conjugate_prior(self) -> "ConjugateFamily":
+        return ConjugateFamily(self)
+
+    def posterior_parameters(
+        self,
+        prior_natural_parameters: ArrayLike | jnp.ndarray,
+        data: ArrayLike | jnp.ndarray,
+    ) -> jnp.ndarray:
+        """Computes the natural parameters of the posterior distribution under the conjugate prior.
+        Signature `(P),(D)->(P)`
+
+        This can be implemented already in the abc and inherited by all subclasses,
+        even if the conjugate log partition function is not available.
+        (In the latter case, only the unnormalized posterior is immediately available, see below.)
+        """
+        prior_natural_parameters = jnp.asarray(prior_natural_parameters)
+        sufficient_statistics = self.sufficient_statistics(data)
+
+        n = sufficient_statistics[..., 0].size
+        expected_sufficient_statistics = jnp.sum(
+            sufficient_statistics,
+            axis=tuple(range(sufficient_statistics.ndim)),
+        )
+
+        alpha_prior, nu_prior = (
+            prior_natural_parameters[:-1],
+            prior_natural_parameters[-1],
+        )
+
+        return jnp.append(alpha_prior + expected_sufficient_statistics, nu_prior + n)
+
+
+class ConjugateFamily(ExponentialFamily):
+    def __init__(self, likelihood: ExponentialFamily) -> None:
+        self._likelihood = likelihood
+
+    @partial(jnp.vectorize, excluded={0}, signature="(d)->(p)")
+    def sufficient_statistics(self, w: ArrayLike | jnp.ndarray, /) -> jnp.ndarray:
+        """Signature `(D)->(P)`
+        The sufficient statistics of the conjugate family are
+        the natural parameters and the (negative) log partition function of the likelihood.
+        """
+        return jnp.append(
+            self._likelihood.parameters_to_natural_parameters(w),
+            -self._likelihood.log_partition(w),
+        )
+
+    def log_base_measure(self, w: ArrayLike | jnp.ndarray, /) -> jnp.ndarray:
+        """Signature `(D)->()`
+        The base measure of the conjugate family is, implicitly, the Lebesgue measure.
+        """
+        w = jnp.asarray(w)
+        return jnp.zeros_like(w[..., 0])
+
+    def log_partition(
+        self, natural_parameters: ArrayLike | jnp.ndarray, /
+    ) -> jnp.ndarray:
+        """Signature `(P)->()`
+        If the conjugate log partition function is available,
+        we can use it to compute the log partition function of the conjugate family.
+        """
+        natural_parameters = jnp.asarray(natural_parameters)
+        alpha, nu = natural_parameters[:-1], natural_parameters[-1]
+        return self._likelihood.conjugate_log_partition(alpha, nu)
+
+    def unnormalized_logpdf(
+        self, w: ArrayLike | jnp.ndarray, natural_parameters: ArrayLike | jnp.ndarray, /
+    ) -> jnp.ndarray:
+        """Signature `(D),(P)->()`
+        Even if the conjugate log partition function is not available,
+        we can still compute the unnormalized log pdf of the conjugate family.
+        """
+        return self.sufficient_statistics(w) @ jnp.asarray(natural_parameters)
